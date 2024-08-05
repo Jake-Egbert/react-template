a530fd44345ac1d3aaa6854b109d2a7b0f261a8a
Jake-Egbert <jake@devpipeline.com>
2024-08-05 15:20:31 -0600

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+import TodoItem from "./TodoItem";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  function handleSumbit(e) {
+    e.preventDefault();
+
+    if (todo.trim() === "") {
+      alert("todo cannot be empty");
+      setTodo("");
+      return;
+    }
+
+    setTodos((prevTodos) => {
+      const newTodo = {
+        id: todos.at(-1)?.id + 1 || 1,
+        title: todo,
+        done: false,
+      };
+      return [...prevTodos, newTodo];
+    });
+    console.log(todos);
+    setTodo("");
+  }
+
+  function handleComplete(id) {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+
+  function handleDelete(id) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
+  function renderTodos() {
+    return todos.map((todo) => {
+      return (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleDelete={handleDelete}
+          handleComplete={handleComplete}
+        />
+      );
+    });
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+
+      <form onSubmit={handleSumbit} className="add-todo">
+        <input
+          type="text"
+          placeholder="Add Todo"
+          onChange={(event) => setTodo(event.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {renderTodos()}
+    </div>
+  );
+}
